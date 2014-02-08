fff1fdefc6dd5c4da5119c292f07850e1dfcd2e7
Eric Palakovich Carr <carreric@gmail.com>
2014-02-07 23:17:25 -0500

    Removed superflous --pythonpath command line arg.

--- pytest_pythonpath.py
+++ pytest_pythonpath.py
@@ -2,12 +2,10 @@ import sys
 
 
 def pytest_addoption(parser):
-    group = parser.getgroup("general")
-    group.addoption('--pythonpath', action='store_true', help="update PYTHONPATH using .ini settings.")
-    parser.addini("python_paths", type="pathlist", help="space seperated directory paths to add to PYTHONPATH")
+    parser.addini("python_paths", type="pathlist", help="space seperated directory paths to add to PYTHONPATH",
+                  default=[])
 
 
 def pytest_configure(config):
-    if config.option.pythonpath:
-        for path in config.getini("python_paths"):
-            sys.path.append(str(path))
+    for path in config.getini("python_paths"):
+        sys.path.append(str(path))
