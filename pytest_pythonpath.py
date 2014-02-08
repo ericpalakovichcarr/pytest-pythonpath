2ff360b6f0e6e9993bc74b497fcabf582110e24e
Eric Palakovich Carr <carreric@gmail.com>
2014-02-08 00:07:43 -0500

    Added py.test fix so cwd is always present in PYTHONPATH.

--- pytest_pythonpath.py
+++ pytest_pythonpath.py
@@ -1,7 +1,11 @@
 import sys
+import os
 
 
 def pytest_addoption(parser):
+    # py.test has an issue where the cwd is not in the PYTHONPATH. Fix it here.
+    if os.getcwd() not in sys.path:
+        sys.path.insert(0, os.getcwd())
     parser.addini("python_paths", type="pathlist", help="space seperated directory paths to add to PYTHONPATH",
                   default=[])
 
