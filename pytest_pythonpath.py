ab64b6ca0f011f1324d70a8ace2fe37ddea37c2e
Cordell Blakkan <cblakkan@23andme.com>
2014-10-11 15:41:07 -0700

    Properly parse .pth files. Modify pythonpath before tests are imported

--- pytest_pythonpath.py
+++ pytest_pythonpath.py
@@ -1,15 +1,16 @@
-import sys
 import os
+import site
+import sys
 
 
 def pytest_addoption(parser):
     # py.test has an issue where the cwd is not in the PYTHONPATH. Fix it here.
     if os.getcwd() not in sys.path:
-        sys.path.insert(0, os.getcwd())
+        site.addsitedir(os.getcwd())
     parser.addini("python_paths", type="pathlist", help="space seperated directory paths to add to PYTHONPATH",
                   default=[])
 
 
-def pytest_configure(config):
-    for path in config.getini("python_paths"):
-        sys.path.append(str(path))
+def pytest_load_initial_conftests(args, early_config, parser):
+    for path in early_config.getini("python_paths"):
+        site.addsitedir(str(path))
