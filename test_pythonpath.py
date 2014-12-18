d9b1f598c33647f7893d121eb628fce722407ee7
Eric Palakovich Carr <carreric@gmail.com>
2014-12-17 23:09:02 -0500

    Added tests to ensure proper PYTHONPATH insertion order.

--- test_pythonpath.py
+++ test_pythonpath.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from test_package import return_hi, hey
 from hello import return_hello
 
@@ -6,3 +9,9 @@ def test_imports_worked():
     assert "hi" == return_hi()
     assert "hey" == hey.return_hey()
     assert "hello" == return_hello()
+
+
+def test_pythonpath_has_correct_order():
+    assert sys.path[0] == os.path.join(os.getcwd())
+    assert sys.path[1] == os.path.join(os.getcwd(), "test_path/apps")
+    assert sys.path[2] == os.path.join(os.getcwd(), "test_path/libs")
