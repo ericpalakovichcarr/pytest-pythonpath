af0f3024485a75d442e56188d517307dafae3bae
Eric Palakovich Carr <eric@victoryguide.us>
2018-08-22 14:18:55 -0400

    Bumped version to 0.7.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description=description,
     long_description=long_description,
     license='MIT',
-    version='0.7.2',
+    version='0.7.3',
     author='Eric Palakovich Carr',
     author_email='carreric@gmail.com',
     url='https://github.com/bigsassy/pytest-pythonpath',
